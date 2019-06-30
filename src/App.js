89b6b9a9b57fd8dee159f8f1d9ebe1f23f28a718
Sanjay Rathod <rsanjay629@gmail.com>
2019-06-30 15:03:10 +0530

    with Proper Comment
    Add Comment for each & every function as well as All Component and Pages
    
    Add Search Functionality in Page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+/**
+ * Create By Sanjay 
+ * This is a Main Component of Application
+ */
 import React, { Component, Fragment } from 'react';
-import { Row, Col, Input } from 'reactstrap';
+import { Row, Col, Input, Label } from 'reactstrap';
+//for Notification porpose on add/edit/delete
 import { NotificationManager, NotificationContainer } from 'react-notifications';
 
+//product table component 
 import ProductTable from "./components/ProductTable";
+//all style import here
 import "./libCss";
 import jsonData from './data';
 
@@ -15,25 +22,29 @@ class App extends Component {
     isAddOpen: false,
     deleteId: "",
     editData: {},
-    addData:{
+    addData: {
       product_id: null,
-      product_name:"",
+      product_name: "",
       product_price: "",
       product_dis: ""
-    }
+    },
+    filtered: [],
+    flag: true
   }
 
+  //when delete Popoup Open 
   toggleDelete = (value) => {
     this.setState({
       isDeleteOpen: true,
       isEditOpen: false,
       deleteId: value,
-      editData:{},
-      isAddOpen:false,
-      addData:{}
+      editData: {},
+      isAddOpen: false,
+      addData: {}
     })
   }
 
+  //edit modal Open 
   toggleEdit = (data) => {
     this.setState({
       isEditOpen: true,
@@ -44,7 +55,8 @@ class App extends Component {
     })
   }
 
-  toggleAdd = () =>{
+  //add Modal Open
+  toggleAdd = () => {
     this.setState({
       isAddOpen: true,
       isEditOpen: false,
@@ -54,6 +66,7 @@ class App extends Component {
     })
   }
 
+  //onClose of all popup modal 
   onClose = () => {
     this.setState({
       isDeleteOpen: false,
@@ -65,8 +78,10 @@ class App extends Component {
     })
   }
 
+  //function for Remove Data from Table  
   removeRecord = () => {
-    const list = this.state.Product_List.slice();
+    const list = this.state.Product_List.slice();//slice the array 
+    //find exact match and delete with splice
     list.some((el, i) => {
       if (el.product_id === this.state.deleteId) {
         list.splice(i, 1);
@@ -85,6 +100,7 @@ class App extends Component {
     NotificationManager.success("successfully deleted record");
   }
 
+  //change event function for edit form 
   onChangeEdit = (e) => {
     var tempObj = Object.assign({}, this.state.editData)
     tempObj[e.target.name] = e.target.value;
@@ -93,12 +109,15 @@ class App extends Component {
     })
   }
 
+  //on Edit Button Click
   onEditData = () => {
+    //find the index of object in array 
     var objIndex = this.state.Product_List.findIndex((obj => obj.product_id === this.state.editData.product_id));
+    //store array in new var and replace index of Array object with new one
     var newObj = this.state.Product_List;
     newObj[objIndex] = this.state.editData;
     this.setState({
-      Product_List : newObj,
+      Product_List: newObj,
       isEditOpen: false,
       isDeleteOpen: false,
       deleteId: "",
@@ -109,6 +128,7 @@ class App extends Component {
     NotificationManager.success("successfully updated record");
   }
 
+  //onChange event Of add Form 
   onChangeAdd = (e) => {
     var tempObj = Object.assign({}, this.state.addData)
     tempObj[e.target.name] = e.target.value;
@@ -118,7 +138,8 @@ class App extends Component {
     })
   }
 
-  onAddData = () =>{    
+  //add button click function
+  onAddData = () => {
     this.setState({
       Product_List: [...this.state.Product_List, this.state.addData],
       isAddOpen: false,
@@ -126,25 +147,55 @@ class App extends Component {
       isEditOpen: false,
       addData: {},
       editData: {},
-      deleteId: "" 
+      deleteId: ""
     })
     NotificationManager.success("successfully added record");
   }
 
+  //on Search Data in Table 
+  onSearchData = (e) => {
+    var searchData = [];
+    if (e.target.value !== "") {
+      function find(arr) {
+        var result = [];
+        for (var i in arr) {
+          if (arr[i].product_name.toLowerCase().match(e.target.value.toLowerCase())) {
+            result.push(arr[i]);
+          }
+        }
+        return result;
+      }
+      searchData = find(this.state.Product_List);
+      this.setState({
+        filtered: searchData,
+        flag: false
+      })
+    } else {
+      searchData = [];
+      this.setState({
+        filtered: searchData,
+        flag: true
+      })
+    }
+  }
+
   render() {
-    const { Product_List, isDeleteOpen, isEditOpen, editData, isAddOpen, addData } = this.state; 
+    const { Product_List, isDeleteOpen, isEditOpen, editData, isAddOpen, addData, filtered, flag } = this.state;
     return (
       <Fragment>
         <Row className="App">
           <Col md="3" className="App-Search">
-            <Input placeholder="Product Search Here....."/>
+            <Input
+              placeholder="Product Search By Name....."
+              onChange={(e) => this.onSearchData(e)}
+            />
           </Col>
           <Col md="9">
-            <ProductTable 
-              removeRecord={this.removeRecord} 
-              isDeleteOpen={isDeleteOpen} 
-              toggleDelete={this.toggleDelete} 
-              Product_List={Product_List} 
+            <ProductTable
+              removeRecord={this.removeRecord}
+              isDeleteOpen={isDeleteOpen}
+              toggleDelete={this.toggleDelete}
+              Product_List={flag ? Product_List : filtered}
               onClose={this.onClose}
               toggleEdit={this.toggleEdit}
               isEditOpen={isEditOpen}
@@ -159,7 +210,7 @@ class App extends Component {
             />
           </Col>
         </Row>
-        <NotificationContainer/>
+        <NotificationContainer />
       </Fragment>
     )
   }
